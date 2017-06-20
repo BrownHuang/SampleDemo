db68ad0a8ea5c78b8f952c8a2998d10eaf688b80
Brown <b9602104@gmail.com>
2017-06-20 22:05:16 +0800

    First commit

---
+++ SampleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleDemo
+//
+//  Created by Bo-Rong Huang on 2017/6/20.
+//  Copyright © 2017年 Bo-Rong Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleDemo
+//
+//  Created by Bo-Rong Huang on 2017/6/20.
+//  Copyright © 2017年 Bo-Rong Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
